b095781317a03195bff0912a26db023711c4f256
OS <nguyenhd.21it@vku.udn.un>
2022-06-13 09:16:07 +0700

    lan gan cuoi

--- assets/script.js
+++ assets/script.js
@@ -19,17 +19,42 @@ function topFunction() {
 const menuIcon = document.querySelector('#mobile-menu');
 const navMoblie = document.querySelector('.nav__mobile_tablet');
 
-menuIcon.addEventListener('click',function(){
+menuIcon.addEventListener('click', function () {
   navMoblie.classList.toggle('open')
 })
 
- // Auto close menu 
- var menuItems = document.querySelectorAll('.nav__mobile_tablet li');
- for (var i = 0 ; i < menuItems.length ; i++){
-     var menuItem = menuItems[i];
-     
-     menuItem.onclick = function(event){
-        navMoblie.classList.remove('open')
-     }
- }
+// Auto close menu 
+var menuItems = document.querySelectorAll('.nav__mobile_tablet li');
+for (var i = 0; i < menuItems.length; i++) {
+  var menuItem = menuItems[i];
 
+  menuItem.onclick = function (event) {
+    navMoblie.classList.remove('open')
+  }
+}
+
+// Zoom image - copy
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.querySelectorAll(".myImg");
+var modalImg = document.getElementById("img01");
+
+for (let i=0;i <img.length ;i++){
+  img[i].onclick = function () {
+    console.log(i);
+    modal.style.display = "block";
+    modalImg.src = img[i].src;
+ 
+
+  }
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
